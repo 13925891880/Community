6f4be42c526001622c090b78a8f60bdc0c017829
xiaojia <1850209479@qq.com>
2022-04-20 11:17:54 +0800

    二次提交

---
+++ src/main/java/com/nowcoder/community/util/CommunityUtil.java
@@ -0,0 +1,39 @@
+package com.nowcoder.community.util;
+
+
+
+import ch.qos.logback.classic.util.LogbackMDCAdapter;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.DigestUtils;
+
+import java.util.Map;
+import java.util.UUID;
+
+public class CommunityUtil {
+    // 生成随机字符串
+    public static String generateUUID(){
+        return UUID.randomUUID().toString().replaceAll("-","");
+    }
+    // MD5加密
+    public static String md5(String key){
+        if (StringUtils.isBlank(key)){
+            return null;
+        }
+        return DigestUtils.md5DigestAsHex(key.getBytes());
+    }
+
+
+    public static String getJSONString(int code, String msg, Map<String, Object> map) {
+
+        return null;
+    }
+
+    public static String getJSONString(int code, String msg) {
+        return getJSONString(code, msg, null);
+    }
+
+    public static String getJSONString(int code) {
+        return getJSONString(code, null, null);
+    }
+}
